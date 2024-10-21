48e6adfc8378001648970bab53334aa14dad03f8
George Pestell <gp87@st-andrews.ac.uk>
2024-10-21 22:23:06 +0100

    Proper projection to UTM

--- scripts/benNevis_overpass.sh
+++ scripts/benNevis_overpass.sh
@@ -11,4 +11,4 @@ nwr[natural='bay'](56.750440999999995,-5.1407810000000005,56.871590999999995,-4.
 
 gdal_rasterize -l lines -l multipolygons -burn 1.0 -tr 0.000277 0.000277 -a_nodata 0.0 -te -5.127361122222 56.761527788889 -4.96513889987 56.864861122305 -ot Float32 -of GTiff ../data/benNevis.xml ../data/benNevis_water.tiff#
 
-gdalwarp ../data/benNevis_water.tiff ../data/benNevis_water.xyz
+gdalwarp -s_srs WGS84 -t_srs '+proj=utm +zone=30 +datum=WGS84 +units=m +no_defs' ../data/benNevis_water.tiff ../data/benNevis_water.xyz
