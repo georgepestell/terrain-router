1a0702b0cfb2c8ee6e4c8a4adec7523bd8ce1af2
George Pestell <gp87@st-andrews.ac.uk>
2024-11-03 20:34:11 +0000

    bugfix: update cmd.cpp

--- src/tsr/cmd.cpp
+++ src/tsr/cmd.cpp
@@ -1,53 +1,26 @@
 #include "tsr/DTM.hpp"
-#include "tsr/IO.hpp"
+#include "tsr/IO/FileIOHandler.hpp"
 #include "tsr/logging.hpp"
 
 #include <cstdlib>
-#include <filesystem>
 #include <memory>
 #include <string>
 #include <vector>
 
 #define DEFAULT_DEM_FILE SOURCE_ROOT "/data/benNevis_UTM.xyz"
 
-using namespace std;
-
 namespace tsr {
 
-bool tsr_run(vector<string> args) {
+bool tsr_run(std::vector<std::string> args) {
 
-  unique_ptr<DTM> dtm;
-
-  // Chcek if a DEM filepath was specified
-  if (args.size() == 1) {
-
-    string filepath = path_to_absolute(args[0].c_str());
+  std::string filepath = args.size() == 1 ? IO::path_to_absolute(args[0]) : DEFAULT_DEM_FILE;
 
     // Construct the DTM from filepath
-    TSR_LOG_TRACE("loading DEM file: ({:s})", filepath);
-    dtm = make_unique<DTM>(load_points_from_file(filepath.c_str()));
+  TSR_LOG_TRACE("loading dem file ({:s})", filepath);
+  auto points = IO::load_dem_from_file(filepath);
 
-  } else {
-
-    // Load default DEM file
-    TSR_LOG_TRACE("loading default DEM ({:s})", DEFAULT_DEM_FILE);
-    dtm = make_unique<DTM>(load_points_from_file(DEFAULT_DEM_FILE));
-  }
-
-  // Fetch a reference to the topology mesh
-  Delaunay_3D &topology_mesh = dtm->get_topology();
-  TSR_LOG_INFO("topology vertices: {:d}", topology_mesh.number_of_vertices());
-
-  // Simplify large regions
-  dtm->simplify_3d_feature(topology_mesh, topology_mesh);
-
-  // Simplify small regions
-  // simplify_3d_mesh(topology_mesh, topology_mesh, 0.70, 0.50, 0.95, 0.5);
-
-  TSR_LOG_INFO("simplified topology vertices: {:d}",
-               topology_mesh.number_of_vertices());
-
-  write_mesh_to_obj("dtm.obj", dtm->get_topology());
+  TSR_LOG_TRACE("creating dtm from point cloud");
+  auto dtm = std::make_unique<DTM>(*points);
 
   return EXIT_SUCCESS;
 }
@@ -56,7 +29,7 @@ bool tsr_run(vector<string> args) {
 
 int main(int argc, char **argv) {
 
-  vector<string> args;
+  std::vector<std::string> args;
   for (int i = 1; i < argc; i++) {
     args.push_back(argv[i]);
   }
