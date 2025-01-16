81302bb4ee4fbbfc4b92dd2c429013cc3a3241e4
George Pestell <gp87@st-andrews.ac.uk>
2025-01-16 15:07:55 +0000

    Add restricted swimming preset

--- include/tsr/Presets.hpp
+++ include/tsr/Presets.hpp
@@ -7,10 +7,15 @@ namespace tsr {
 FeatureManager SetupTimePreset(Tin &tin, const MeshBoundary &boundary);
 FeatureManager SetupTimeWithSwimmingPreset(Tin &tin,
                                            const MeshBoundary &boundary);
+FeatureManager SetupTimeRestrictSwimmingPreset(Tin &tin,
+                                               const MeshBoundary &boundary);
+
+/// Simply minimizes the overall distance travelled
 FeatureManager SetupSimpleDistancePreset(Tin &tin,
                                          const MeshBoundary &boundary);
-// FeatureManager SetupTerrainTypePreset(Tin &tin, const MeshBoundary
-// &boundary);
+
+/// This feature minimizes the overall elevation change (i.e. distance *
+/// abs(gradient))
 FeatureManager SetupSimpleGradientPreset(Tin &tin,
                                          const MeshBoundary &boundary);
 
